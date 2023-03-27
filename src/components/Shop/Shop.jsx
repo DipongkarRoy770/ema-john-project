08ea9b3824221451e4cf855fc69d2808607f8d2a
DipongkarRoy770 <dipongkarroy233@gmail.com>
2023-03-27 15:35:09 +0600

    done project

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,11 +4,19 @@ import './Shop.css'
 
 const Shop = () => {
   const [data ,setData] =useState([])
+  const [card ,setCard] =useState([])
   useEffect(()=>{
     fetch('products.json')
      .then(res =>res.json())
      .then(data=>setData(data))
-  },[])
+  },[]);
+  
+  const handleAddToCard=(product)=>{
+    // console.log(product)
+    const newCard = [...card ,product]
+    setCard(newCard)
+  };
+
   return (
     <div className='shop-container'>
       <div className="card-container">
@@ -16,11 +24,16 @@ const Shop = () => {
           data.map(product=><SingleData
           key={product.id}
           product ={product}
+          handleAddToCard={handleAddToCard}
           ></SingleData>)
         }
       </div>
       <div className="product-container">
-        <p>oder summary</p>
+        <p className='order'>Order summary</p>
+        <hr className='hr' />
+        <div className='product-bio'>
+           <p>product item selected :{card.length}</p>
+        </div>
       </div>
     </div>
   );
--- src/components/SingleData/SingleData.jsx
+++ src/components/SingleData/SingleData.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './SingleData.css'
 
 const SingleData = (props) => {
-  console.log(props)
   const {img,name,price,ratings,seller} =props.product
+  const handleAddToCard =props.handleAddToCard
+ 
   return (
     <div className='product'>
       <img className='img' src={img} alt="load" />
@@ -13,7 +16,9 @@ const SingleData = (props) => {
        <p>Manufacturer:{seller}</p>
        <p>Ratings:{ratings}</p>
        </div>
-       <button className='btn'>ADD To Card</button>
+      <button onClick={()=>handleAddToCard(props.product)} className='btn'>ADD To Card
+      <FontAwesomeIcon icon={faShoppingCart} />
+      </button>
     </div>
   );
 };
